598d2fa62653bf185dc601b9ecfbf47e9f0ce952
peek <sar.srijan@gmail.com>
2024-08-24 13:43:40 +0530

    connected to db and added tables

---
+++ employee_register/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def employee_list(request):
+    return
+
+def employee_form(request):
+    return
+
+def employee_delete(request):
+    return
--- zemuriaCRUD/asgi.py
+++ zemuriaCRUD/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for projectname project.
+ASGI config for zemuriaCRUD project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectname.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zemuriaCRUD.settings')
 
 application = get_asgi_application()
