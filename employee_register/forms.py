0f200f6fa89c8b6570e54c6f386e383f4cee917d
peek <sar.srijan@gmail.com>
2024-08-26 01:57:37 +0530

    feat:form data submitted to database

--- employee_register/forms.py
+++ employee_register/forms.py
@@ -10,3 +10,7 @@ class EmployeeForm(forms.ModelForm):
             'emp_code'  :  'employee code'
         }
 
+    def __init__(self, *args, **kwargs):
+        super(EmployeeForm, self).__init__(*args, **kwargs)
+        self.fields['position'].empty_label = "select"  
+        self.fields['emp_code'].required = False
--- employee_register/views.py
+++ employee_register/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import EmployeeForm
 
+#view functions
 def employee_list(request):
     return render(request, "employee_register/employee_list.html")
 
 def employee_form(request):
     #this variable will be included in employee_form.html 
-    form = EmployeeForm() #empty form to rendered
-    return render(request, "employee_register/employee_form.html", {'form': form})
-
+    if request.method == "GET":          
+        form = EmployeeForm() #empty form to rendered
+        return render(request, "employee_register/employee_form.html", {'form': form})
+    else:
+        form = EmployeeForm(request.POST) #request.POST is  dict like object called 'QueryDict'
+        if form.is_valid():
+            form.save()
+        return redirect('/employee/list')
+        
 def employee_delete(request):
     return
