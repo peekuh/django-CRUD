fa979ab7e159d224c720257f6745698b5a497a6d
peek <75615590+peekuh@users.noreply.github.com>
2024-08-27 13:14:12 +0530

    Update settings.py

--- zemuriaCRUD/settings.py
+++ zemuriaCRUD/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'EmployeeDB',
         'USER' : 'postgres',
-        'PASSWORD' : 'Gungun2711!',
+        'PASSWORD' : '***',
         'HOST' : 'localhost',
         'PORT' : '5432'
     }
